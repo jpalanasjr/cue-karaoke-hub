b8dcbcecf962a119beb939f27ee9f096e74f8e59
Julius Palanas Jr <jpalanasjr@gmail.com>
2026-05-16 03:07:59 +0800

    fix path

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <AuthProvider>
           <BookingProvider>
             <Toaster />
